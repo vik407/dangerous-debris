2d443dc307582571ddac65d99ead952c5c156dd8
Victor Hernandez <end35203@adobe.com>
2023-07-07 14:45:09 -0500

    view-transition Demo creation

---
+++ src/data/books.js
@@ -0,0 +1,44 @@
+export const books = [
+ {
+   id: 'clean-code',
+   title: 'Clean Code: Manual de desarrollo ágil',
+   img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+   author: 'Robert C. Martin',
+   description: 'Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.'
+ },
+ {
+   id: 'clean-architecture',
+   title: 'Clean Architecture: A Craftsman\'s Guide to Software Structure and Design',
+   img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1505400487i/36239393.jpg",
+   author: 'Robert C. Martin',
+   description: 'Practical Software Architecture Solutions from the Legendary Robert C. Martin (“Uncle Bob”) By applying universal rules of software architecture, you can dramatically improve developer productivity throughout the life of any software system.'
+ },
+ {
+   id: 'the-clean-coder',
+   title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+   img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+   author: 'Robert C. Martin',
+   description: 'Programmers who endure and succeed amidst swirling uncertainty and nonstop pressure share a common attribute: They care deeply about the practice of creating software. They treat it as a craft. They are professionals. In The Clean Coder: A Code of Conduct for Professional Programmers, legendary software expert Robert C. Martin introduces the disciplines, techniques, tools, and practices of true software craftsmanship. This book is packed with practical advice–about everything from estimating and coding to refactoring and testing. It covers much more than technique: It is about attitude. Martin shows how to approach software development with honor, self-respect, and pride; work well and work clean; communicate and estimate faithfully; face difficult decisions with clarity and honesty; and understand that deep knowledge comes with a responsibility to act.'
+ },
+ {
+   id: 'javascript-the-good-parts',
+   title: 'JavaScript: The Good Parts',
+   img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+   author: 'Douglas Crockford',
+   description: 'Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.'
+ },
+ {
+   id: 'eloquent-javascript',
+   title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+   img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+   author: 'Marijn Haverbeke',
+   description: 'JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.'
+ },
+ {
+   id: 'javascript-patterns',
+   title: 'Javascript Patterns: Build Better Applications with Coding and Design Patterns',
+   img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344668503i/9422683.jpg",
+   author: 'Stoyan Stefanov',
+   description: 'What\'s the best approach for developing an application with JavaScript? This book helps you answer that question with numerous JavaScript coding patterns and best practices. If you\'re an experienced developer looking to solve problems related to objects, functions, inheritance, and other language-specific categories, the abstractions and code templates in this guide are ideal—whether you\'re using JavaScript to write a client-side, server-side, or desktop application.'
+ }
+]
